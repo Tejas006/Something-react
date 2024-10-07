5342137771b2ffa74a7f4fda109515480c233c03
Tejas006 <93635081+Tejas006@users.noreply.github.com>
2024-10-07 15:50:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+//import Home from "./components/home.js";
+import { Counter } from "./components/home.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
